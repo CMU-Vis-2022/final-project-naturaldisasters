80d8a7f7c98f6c93892f6d76b52bf690675271a9
weiyuanz <112225096+weiyuanz@users.noreply.github.com>
2022-11-27 23:18:37 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  build: {
-    target: ["chrome100", "safari15", "firefox100"],
-  },
-  // Change this to your repo name
-  // base: "/D3-App-Template/",
-  base: "/assignment-3-zhang-li/",
-};
